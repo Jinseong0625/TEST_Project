8eba23581a03193dcb493b06a452add20d11c617
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2023-10-27 15:05:02 +0900

    Revert "Merge branch 'local_branch'"
    This reverts commit fdf7e8b43e222864ea9c6735b033eb028f62bb14, reversing
    changes made to d6654444ff420eb0ed275557d682639b310f52ba.

--- DBHandler.php
+++ DBHandler.php
@@ -6,35 +6,6 @@ use DBConnector;
 use Firebase\JWT\JWT;
 
 class DBHandler extends DBConnector{
-
-    public function sp_select_User($uid)
-    {
-        $error = "E0000";
-
-        if (!($stmt = $this->db->prepare("CALL sp_select_User(?)"))) {
-            $error = "E1000"; // Prepare failed
-        }
-        if (!$stmt->bind_param("s", $uid)) {
-            $error = "E1001"; // Bind failed
-        }
-        if (!$stmt->execute()) {
-            $error = "E1002"; // Execute failed
-        }
-
-        $res = $stmt->get_result();
-        $data = array();
-
-        while($row = $res->fetch_assoc()){
-            $data[] = $row;
-        }
-
-        $json_data = array(
-            "error" => $error,
-            "data" => $data
-        );
-
-        return json_encode($json_data);
-    }
     
 }
 
