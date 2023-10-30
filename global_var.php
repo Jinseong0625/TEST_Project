075929befe1e6bf343c8a54cb1a4474afff943be
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2023-10-30 12:07:09 +0900

    DB 커넥트 수정

--- global_var.php
+++ global_var.php
@@ -1,7 +1,7 @@
 <?php
      define("_Host_", "218.38.65.83");  //Live
-     define("_User_", "root");
-     define("_Pwd_", "1234");
+     define("_User_", "sysadmin1");
+     define("_Pwd_", "Newto123");
      define("_Db_" , "test");
      define("_Port_" ,"3306");
 
